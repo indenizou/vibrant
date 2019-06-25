16b6da30d09d8ac0571b943a5d5d5a6863e20736
Christhopher Lion <christhopherleao@icloud.com>
2019-06-25 14:17:03 +0100

    fix container

--- src/components/Pipeline/Pipeline.jsx
+++ src/components/Pipeline/Pipeline.jsx
@@ -3,18 +3,20 @@ import './Pipeline.scss';
 import pipeline from './steps-data';
 
 const Pipeline = (
-  <section id="Pipeline" className="column contain p2" mobile-align="start center">
-    {/* <h3 mobile-flex="80" className="mt1 mb1">Passo a Passo:</h3> */}
-    <h2 className="mb1">Passo a Passo:</h2>
+  <section id="Pipeline" >
+    <div className="column contain p2" mobile-align="start center">
+      {/* <h3 mobile-flex="80" className="mt1 mb1">Passo a Passo:</h3> */}
+      <h2 className="mb1">Passo a Passo:</h2>
 
-    <ol align="around start" className="pt1 pb2 row">
-      {pipeline.map(({ step, description, icon }) => (
-        <li flex="20" mobile-flex="80" key={step}>
-          <h6>{step}</h6>
-          <p>{description}</p>
-        </li>
-      ))}
-    </ol>
+      <ol align="around start" className="pt1 pb2 row">
+        {pipeline.map(({ step, description, icon }) => (
+          <li flex="20" mobile-flex="80" key={step}>
+            <h6>{step}</h6>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
   </section>
 );
 
